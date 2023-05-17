91eb383809949912f616f1841a2bf50cbbcc02e6
Min Jae <mjkweon17@korea.ac.kr>
2023-05-17 17:27:01 +0900

    23/05/07 파이썬 input 연습

--- t.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-read = sys.stdin.readline
-N, M, K = map(int, read().split())
--- test.py
+++
@@ -1,5 +0,0 @@
-import sys
-n1 = int(sys.stdin.readline())
-data2 = [sys.stdin.readline().strip() for i in range(n1)]
-for str in data2:
-    print(str)
