98ea502b8bc22072169ea6c7185a960fae2e5928
Min Jae <mjkweon17@korea.ac.kr>
2023-05-23 00:12:58 +0900

    23/05/21 백준-파이썬-단계-심화 1

--- 2023_BAEKJOON_Python/06. 심화 1 단계/25206.py
+++ 2023_BAEKJOON_Python/06. 심화 1 단계/25206.py
@@ -4,10 +4,16 @@ read = sys.stdin.readline
 
 dict = { "A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0}
 
-total_grade = 0
-total_weight = 0
+total_weight = 0    #전체 학점
+total_grade = 0     #등급 총합
 for _ in range(20):
     class_name, weight, grade = map(str.strip, read().split())
-    total_weight += float(weight)
-    total_grade += float(weight) + dict[grade]
-print(total_grade / total_weight)
+    weight = float(weight)  #학점
+    if grade == 'P':
+        continue
+    total_weight += weight
+    total_grade += dict[grade] * weight
+if total_weight == 0:
+    print(format(0, ".6f"))
+else:
+    print(round(total_grade / total_weight, 6))
